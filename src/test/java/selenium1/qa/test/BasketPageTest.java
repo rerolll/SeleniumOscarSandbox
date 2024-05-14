52918b6dd98d4212a091ed8b250d81a3371f4e39
IlyaHomePC <kosmakovie@gmail.com>
2024-05-14 21:49:43 +0300

    code optimization

--- src/test/java/selenium1/qa/test/BasketPageTest.java
+++ src/test/java/selenium1/qa/test/BasketPageTest.java
@@ -5,43 +5,54 @@ import gurock.testrail.APIException;
 import org.openqa.selenium.support.PageFactory;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import selenium1.qa.pages.helper.BasketPageHelper;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class BasketPageTest extends BaseTest {
     BasketPageHelper basketPageHelper;
+    String TESTRAILRUNID = "2";
+    String testCaseId;
 
     @BeforeMethod
-    public void initTests() {
+    public void initTests(Method method) {
         basketPageHelper = PageFactory.initElements(driver, BasketPageHelper.class);
+        Test testAnnotation = method.getAnnotation(Test.class);
+        testCaseId = testAnnotation.description();
     }
 
-    @Test
+    @AfterMethod
+    public void addTestResult(ITestResult result) throws APIException, IOException {
+        int resultStatus;
+        if (result.isSuccess()) {
+            resultStatus = TEST_CASE_PASSED_STATUS;
+        } else {
+            resultStatus = TEST_CASE_FAILED_STATUS;
+        }
+        BaseTest.AddResultForTestCaseInTestRail(TESTRAILRUNID, testCaseId, resultStatus);
+    }
+
+    @Test(description = "4")
     public void add_to_basket() throws APIException, IOException {
         basketPageHelper.btn_add_to_basket_third_product.click();
         String added_product_name = basketPageHelper.name_of_third_product.getText();
-
-        boolean isAddedNameIsCorrect = basketPageHelper.name_of_added_product_in_allert.getText().contains(added_product_name);
         String added_product_price = basketPageHelper.price_of_third_product.getText();
+        Assert.assertTrue((basketPageHelper.name_of_added_product_in_allert.getText().contains(added_product_name)),
+                "Имя продукта добавленного в корзину не совпадает с ожидаемым");
+
         basketPageHelper.basket_link.click();
 
-        boolean isNameInBasketCorrect = basketPageHelper.name_first_product_in_basket.getText().contains(added_product_name);
-        boolean isPriceInBasketIsCorrect = basketPageHelper.price_first_product_in_basket.getText().contains(added_product_price);
+        Assert.assertTrue((basketPageHelper.price_first_product_in_basket.getText().contains(added_product_price)),
+                "Цена продукта в корзине не совпадает с добавленным");
+        Assert.assertTrue((basketPageHelper.name_first_product_in_basket.getText().contains(added_product_name)),
+                "Имя продукта в корзине не совпадает с добавленным");
 
-        boolean isAdded = isAddedNameIsCorrect && isNameInBasketCorrect && isPriceInBasketIsCorrect;
-        if (isAdded) {
-            BaseTest.AddResultForTestCaseInTestRail("2","4", TEST_CASE_PASSED_STATUS);
-        } else {
-            BaseTest.AddResultForTestCaseInTestRail("2","4", TEST_CASE_FAILED_STATUS);
-        }
-        Assert.assertTrue((isPriceInBasketIsCorrect), "Цена продукта в корзине не совпадает с добавленным");
-        Assert.assertTrue((isNameInBasketCorrect), "Имя продукта в корзине не совпадает с добавленным");
-        Assert.assertTrue((isAddedNameIsCorrect), "Имя продукта добавленного в корзину не совпадает с ожидаемым");
+
     }
-
-
 }
--- src/test/java/selenium1/qa/test/LoginPageTest.java
+++ src/test/java/selenium1/qa/test/LoginPageTest.java
@@ -5,6 +5,8 @@ import gurock.testrail.APIException;
 import org.openqa.selenium.support.PageFactory;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,55 +15,61 @@ import selenium1.qa.pages.helper.LoginPageHelper;
 import selenium1.qa.pages.resources.TakeJsonData;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class LoginPageTest extends BaseTest {
     LoginPageHelper loginPageHelper;
+    String TESTRAILRUNID = "1";
+    String testCaseId;
 
 
     @BeforeMethod
-    public void initTests() {
+    public void initTests(Method method) {
         loginPageHelper = PageFactory.initElements(driver, LoginPageHelper.class);
         driver.manage().deleteAllCookies();
         driver.navigate().refresh();
         loginPageHelper.go_to_login_page();
         loginPageHelper.should_be_login_url();
+
+        Test testAnnotation = method.getAnnotation(Test.class);
+        testCaseId = testAnnotation.description();
+
+    }
+    @AfterMethod
+    public void addTestResult(ITestResult result) throws APIException, IOException {
+        int resultStatus;
+        if(result.isSuccess()) {
+            resultStatus = TEST_CASE_PASSED_STATUS;
+        } else {
+            resultStatus = TEST_CASE_FAILED_STATUS;
+        }
+
+        BaseTest.AddResultForTestCaseInTestRail(TESTRAILRUNID, testCaseId, resultStatus);
     }
 
-    @Test
+    @Test(description = "1")
     public void registerNewUser() throws APIException, IOException {
+        String testCaseId = "1";
         loginPageHelper.registr_email.sendKeys(LoginPageHelper.setValid_email());
         String password = LoginPageHelper.setValid_password();
         loginPageHelper.registr_pswd1.sendKeys(password);
         loginPageHelper.registr_pswd2.sendKeys(password);
         loginPageHelper.registr_btn.click();
-
-        boolean isUserRegistered = loginPageHelper.succes_alert_of_register.getText().contains("Спасибо за регистрацию!");
-        if(isUserRegistered){
-            BaseTest.AddResultForTestCaseInTestRail("1", "1", TEST_CASE_PASSED_STATUS);
-        } else {
-            BaseTest.AddResultForTestCaseInTestRail("1", "1", TEST_CASE_FAILED_STATUS);
-        }
-        Assert.assertTrue(isUserRegistered);
+        // Allure
+        Assert.assertTrue(loginPageHelper.succes_alert_of_register.getText().contains("Спасибо за регистрацию!"));
     }
 
-    @Test
+    @Test(description = "2")
     public void loginInPresentUser() throws APIException, IOException {
         String valid_email = TakeJsonData.take_json_data("valid_email");
         String valid_password = TakeJsonData.take_json_data("valid_password");
         loginPageHelper.login_email.sendKeys(valid_email);
         loginPageHelper.login_password.sendKeys(valid_password);
         loginPageHelper.login_btn.click();
-
-        boolean isUserLoginIn = loginPageHelper.succes_alert_of_login_in.getText().contains("Рады видеть вас снова");
-        if(isUserLoginIn){
-            BaseTest.AddResultForTestCaseInTestRail("1","2", TEST_CASE_PASSED_STATUS);
-        } else {
-            BaseTest.AddResultForTestCaseInTestRail("1","2", TEST_CASE_FAILED_STATUS);
-        }
-        Assert.assertTrue(isUserLoginIn);
+        Assert.assertTrue(loginPageHelper.succes_alert_of_login_in.getText().contains("Рады видеть вас снова"));
     }
 
-    @Test
+    @Test(description = "3")
     public void loginInWithInvalidPassword() throws APIException, IOException {
         String valid_email = TakeJsonData.take_json_data("valid_email");
         String invalid_password = LoginPageHelper.setValid_password();
@@ -69,13 +77,7 @@ public class LoginPageTest extends BaseTest {
         loginPageHelper.login_password.sendKeys(invalid_password);
         loginPageHelper.login_btn.click();
 
-        boolean isUserLoginIn = loginPageHelper.succes_alert_of_login_in.getText().contains("Рады видеть вас снова");
-        if (!isUserLoginIn) {
-            BaseTest.AddResultForTestCaseInTestRail("1","3", TEST_CASE_PASSED_STATUS);
-        } else {
-            BaseTest.AddResultForTestCaseInTestRail("1","3", TEST_CASE_FAILED_STATUS);
-        }
-        Assert.assertFalse(isUserLoginIn);
+        Assert.assertFalse(loginPageHelper.succes_alert_of_login_in.getText().contains("Рады видеть вас снова"));
     }
 
 }
